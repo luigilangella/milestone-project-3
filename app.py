f81d3f87c3f334aef9f9b57a049fa37f0184a223
Luigi Langella <luigi76langella@gmail.com>
2019-08-12 15:45:41 +0100

    added header, navbar and footer to the base page.

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'expense-tracker'
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_URI"] = 'mongodb+srv://luigi76langella:Marilena7376@myfirstcluster-m2dp9.mongodb.net/expense-tracker?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 @app.route('/')
-def test():
-    return 'Hello World'
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html',  categories=mongo.db.categories.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
