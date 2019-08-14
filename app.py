30b27683cd2bf43c784b19f0aae7911b3f598850
Luigi Langella <luigi76langella@gmail.com>
2019-08-14 20:02:44 +0100

    added javascript functionality to draw graphs and a small script to retrive data from mongoDB in a json format

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -10,12 +11,21 @@ app.config["MONGO_URI"] = 'mongodb+srv://luigi76langella:Marilena7376@myfirstclu
 
 mongo = PyMongo(app)
 
+data = dumps(mongo.db.categories.find())
+
+
+with open('static/data.json', 'w') as file:  # Use file to refer to the file object
+
+   file.write(data)
+
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html',  categories=mongo.db.categories.find())
+    all_categories = mongo.db.categories.find()
+    return render_template('dashboard.html',  categories=all_categories)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
             debug=True)
+    url_for('static', filename='data.json')
---
+++ static/js/graph.js
@@ -0,0 +1,48 @@
+queue()
+    .defer(d3.json, "static/data.json")
+    .await(makegraphs);
+
+function makegraphs(error, data) {
+    var ndx = crossfilter(data);
+
+    pieChart_expense(ndx);
+    barChart_expense(ndx);
+
+    dc.renderAll();
+}
+
+function pieChart_expense(ndx) {
+    var dim = ndx.dimension(dc.pluck("category_name"));
+    var group = dim.group().reduceSum(function(d) {
+        return d.value;
+    });
+
+
+    dc.pieChart("#pie")
+        .width(900)
+        .height(300)
+        .dimension(dim)
+        .group(group)
+        .legend(dc.legend().x(0).y(0).gap(25));
+
+
+}
+
+function barChart_expense(ndx) {
+    var dim = ndx.dimension(dc.pluck("category_name"));
+    var group = dim.group().reduceSum(function(d) {
+        return d.value;
+    });
+
+    dc.barChart("#bar")
+        .width(600)
+        .height(300)
+        .outerPadding(50)
+        .dimension(dim)
+        .group(group)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .yAxis().ticks(5);
+
+
+}
